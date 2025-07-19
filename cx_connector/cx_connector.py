7b636470a23a01cb3b98f5c230f382579227eb96
Vuong Nguyen <vuong.qnguyen10@gmail.com>
2025-07-19 12:24:22 +0700

    connect to ccxt

--- cx_connector/cx_connector.py
+++ cx_connector/cx_connector.py
@@ -1,2 +1,21 @@
+import ccxt
+
+binance = ccxt.binance({})
+
+
+def ticker_ohlcv(symbol, timeframe):
+    """
+    Fetches the ticker price for a given symbol and timeframe.
+
+    :param symbol: The trading pair symbol (e.g., 'SOL/USDT').
+    :param timeframe: The timeframe for the OHLCV data (e.g., '1h', '30m').
+    :return: A list of dictionaries containing the OHLCV data.
+    """
+    ohlcv = binance.fetch_ohlcv(symbol, timeframe)
+    return ohlcv
+
+
 def hello_world():
-    return {"message": "Hello, world!"}
+    print("Hello, world!")
+    data = ticker_ohlcv("SOL/USDT", "1h")
+    return {"data": data}
