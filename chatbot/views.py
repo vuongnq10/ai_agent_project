575c96f94536d861204a279aba004134754fc1b0
Vuong Nguyen <vuong.qnguyen10@gmail.com>
2025-07-19 10:44:24 +0700

    update

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,15 +1,19 @@
+# 1. N8N
+# 2. Langchain
+# 3. LLMs
+
 from django.http import JsonResponse
-import json
 import os
 import google.generativeai as genai
 
-GEMINI_MODEL = "gemini-2.5-flash"
+GEMINI_MODEL = "gemini-1.5-flash"
 API_KEY = os.getenv("GOOGLE_API_KEY")
 
 
 def chat(request):
     try:
         user_message = request.GET.get("query")
+        print(f"Received user message: {user_message}")
 
         if not user_message:
             return JsonResponse({"error": "Message not provided"}, status=400)
@@ -32,3 +36,30 @@ def chat(request):
 
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
+
+
+# def chat(request):
+#     try:
+#         user_message = request.GET.get("query")
+#         print(f"Received user message: {user_message}")
+
+#         if not user_message:
+#             return JsonResponse({"error": "Message not provided"}, status=400)
+
+#         # Configure the Generative AI model
+
+#         if not API_KEY:
+#             return JsonResponse(
+#                 {"error": "GOOGLE_API_KEY environment variable not set"}, status=500
+#             )
+
+#         llm = ChatGoogleGenerativeAI(model=GEMINI_MODEL, google_api_key=API_KEY)
+
+#         # Send message to the Generative AI model
+#         response = llm.invoke([HumanMessage(content=user_message)])
+
+#         ai_message = response.content
+#         return JsonResponse({"message": ai_message})
+
+#     except Exception as e:
+#         return JsonResponse({"error": str(e)}, status=500)
